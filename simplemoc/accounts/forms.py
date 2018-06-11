f85184e6323bcf9aad8b5647a5268dd105511149
Daniel Fernando Catto <danielcatto@hotmail.com>
2018-06-11 19:06:26 -0300

    modificado e comentado aquivo form e views do app accounts

--- simplemoc/accounts/forms.py
+++ simplemoc/accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-class RegistrerForm(UserCreationForm):
+#
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(label='E-mail')
 
--- simplemoc/accounts/views.py
+++ simplemoc/accounts/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
-from .forms import RegistrerForm
+from .forms import RegisterForm
 # Create your views here.
 
-
+#usamas a classe RegisterForm que herda de UserCreationForm criado no forms.py que tem os formulario de autenticação do django e
+# acrecentamos o email ou (alguma informação a mais)
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        #antes era
+        #form = UserCreationForm(request.POST)
+        #e foi criado arquivo forms e criado a class RegistrerForm que herda de registerForms
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     context = {
         'form': form
