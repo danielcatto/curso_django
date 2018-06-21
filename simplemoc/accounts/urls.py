112e9fa77e92c199dbacf59b28d9617aa7d8b6c5
Daniel Fernando Catto <danielcatto@hotmail.com>
2018-06-21 01:21:49 -0300

    modificado forms de account e criado classe EditAccountsForm em views

--- simplemoc/accounts/urls.py
+++ simplemoc/accounts/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^entrar$', 'django.contrib.auth.views.login',
+    url(r'^$', 'simplemoc.accounts.views.dashboard',
+        name='dashboard'),
+
+    url(r'^entrar/$', 'django.contrib.auth.views.login',
         {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^sair/$', 'django.contrib.auth.views.logout',
+        {'next_page': 'core:home'}, name='logout'),
+    url(r'^cadastre-se/$', 'simplemoc.accounts.views.register',name='register'),
+    url(r'^editar/$', 'simplemoc.accounts.views.edit',name='edit'),
 
-    url(r'^cadastre-se', 'simplemoc.accounts.views.register',name='register'),
-
-
-    url(r'^sair$', 'django.contrib.auth.views.login',
-        {'template_name': 'accounts/logout.html'}, name='logout'),
 )
+
--- simplemoc/accounts/views.py
+++ simplemoc/accounts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.conf import settings
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
+
 
 #usamas a classe RegisterForm que herda de UserCreationForm criado no forms.py que tem os formulario de autenticação do django e
 # acrecentamos o email ou (alguma informação a mais)
@@ -14,12 +21,21 @@ def register(request):
         #e foi criado arquivo forms e criado a class RegistrerForm que herda de registerForms
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(username = user.username, password=form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('core:home')
     else:
         form = RegisterForm()
 
     context = {
         'form': form
     }
+    return render(request, template_name, context)
+
+def edit(request):
+    template_name = 'accounts/edit.html'
+    form = EditAccountForm()
+    context = {}
+    context['form'] = form
     return render(request, template_name, context)
